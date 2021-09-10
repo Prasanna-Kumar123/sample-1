b7723e09d948e45b0ba48f92aca4a4599d6e8634
Prasanna-Kumar123 <86958523+Prasanna-Kumar123@users.noreply.github.com>
2021-09-10 18:40:20 +0530

    added for loop

--- main.java
+++ main.java
@@ -2,6 +2,10 @@ class Main
 {
   public static void main(String[] args)
   {
+    for(int i=1;i<=10;i++)
+    {
+      System.out.println(i);
+    }
     System.out.println("Welcome to github");
   }
 }
